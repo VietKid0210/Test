66eb2c5e99a8161837a213f43f4a481626225ca0
VietKid0210 <vietdz20021002@gmail.com>
2024-04-30 16:09:49 +0700

    Test

---
+++ task1.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cin >> a >> b;
+    cout << "Tong hai so la: " << a+b;
+    return 0;
+
+}
---
+++ task2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main(){
+    
+    int sum=0;
+    for(int i=0;i<=10;i++){
+         sum+=i;
+    }
+    cout << sum;
+    return 0;
+}
